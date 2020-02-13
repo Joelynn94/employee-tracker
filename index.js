3512e1760f9a034305bf887e14a999f55a16923b
Joe Lynn <josephlynn94@gmail.com>
2020-02-12 21:56:17 -0500

    added all of the functions to view all of the different SQL tables

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const express = require('express');
 
+// Sets up the 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Create the connection to mysql database
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: "",
+  password: "June199$",
   database: 'employee_DB'
 });
 
+// Connecting to the database 
 connection.connect((err) => {
   if (err) {
     throw err
@@ -22,6 +25,7 @@ connection.connect((err) => {
   startPrompts();
 });
 
+// Starts the user prompts using inquire
 function startPrompts() {
   inquirer
     .prompt({
@@ -38,8 +42,9 @@ function startPrompts() {
         'Update Employee Role'
       ]
     })
-    .then((answer) => {
-      switch (answer.action) {
+    .then(({ action }) => {
+      // checks what the user selects and then runs a function based on user input
+      switch (action) {
         case 'Add Employee':
           addEmployee();
           break;
@@ -72,53 +77,61 @@ function startPrompts() {
 }; 
 
 function addEmployee() {
-  inquirer
-    .prompt([
-      {
-        name: 'firstName',
-        type: 'input',
-        message: "What is the employee's first name?"
-      },
-      {
-        name: 'lastName',
-        type: 'input',
-        message: "What is the employee's last name?"
-      },
-      {
-        name: 'employeeRole',
-        type: 'list',
-        message: "What is the employee's role?",
-        choices: [
-          'Lead Engineer',
-          'Software Engineer',
-          'Salesman',
-          'Marketing Coordinator',
-          'Accountant'
-        ]
-      },
-      {
-        name: 'employeeManager',
-        type: 'input',
-        message: "Who is the employee's manager?"
-        // how do I get the existing managers? 
-      }
-    ])
-    .then(function({ firstName, lastName, employeeRole }) {
-      connection.query('INSERT INTO employee SET ?', 
-      {
-        first_name: firstName,
-        last_name: lastName
-      },
-      function(err) {
-        if (err) {
-          throw err
-        }
-        console.log('Your employee was created successfully!')
-      });
-      startPrompts()
-    })
-}
+  // Gets the role table
+  connection.query('SELECT * FROM role', function(err, result){
+    if (err) throw err;
 
+    connection.query(`SELECT employee.first_name, employee.last_name, employee.id, role.title
+                      FROM role
+                      INNER JOIN employee ON role.id = employee.role_id`, function(err, managerResult){
+                        if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            name: 'firstName',
+            type: 'input',
+            message: "What is the employee's first name?"
+          },
+          {
+            name: 'lastName',
+            type: 'input',
+            message: "What is the employee's last name?"
+          }
+          // {
+          //   name: 'employeeRole',
+          //   type: 'list',
+          //   message: "What is the employee's role?",
+          //   // creates a new array that returns the array of role title's
+          //   choices: result.map(role => role.title)
+          // }
+          // {
+          //   name: 'employeeManager',
+          //   type: 'list',
+          //   message: "Who is the employee's manager?",
+          //   choices: managerResult.map(manager => manager.first_name + ' ' + manager.last_name)
+          // }
+        ])
+        .then(({ firstName, lastName, employeeRole }) => {
+          findRoleId = result.find(role => role.title == employeeRole)
+
+          connection.query('INSERT INTO employee SET ?', 
+          {
+            first_name: firstName,
+            last_name: lastName
+            // role_id: findRoleId
+            // manager_id: employeeManager
+          },
+          function(err) {
+            if (err) {
+              throw err
+            }
+            console.log('Your employee was created successfully!')
+            startPrompts()
+          });
+        });
+      })
+  });
+}
 
 
 function addDepartment() {
@@ -128,10 +141,57 @@ function addDepartment() {
       type: 'input',
       message: 'What department would you like to add?'
     })
-    .then((answer)=> {
+    .then(({ addDepartment })=> {
+      // Inserts what the user entered - into the department table as a new department 
+      connection.query('INSERT INTO department SET ?', { department_name: addDepartment });
+      // Selects all of the columns for the department table
+      connection.query('SELECT * FROM department', function(err, result) {
+        // if there is an error, throw the error 
+        if(err) throw err;
+        // Prints out the sql table in the console
+        console.table(result);
+        // Starts the user prompts
+        startPrompts();
+      });
+    });
+};
 
-      startPrompts();
-    })
+function viewEmployees() {
+  // Selects all of the columns from the employee table
+  connection.query('SELECT * FROM employee', function(err, result){
+    // if there is an error, throw the error
+    if(err) throw err;
+    // prints the current amount of employees
+    console.log('There are: ' + res.length + ' total employees');
+    // Prints out the SQL employee table in the console
+    console.table(result);
+    // Starts the user prompts
+    startPrompts();
+  });
+};
+
+function viewDepartments() {
+  // Selects all of the columns from the department table 
+  connection.query('SELECT * FROM department', function(err, result){
+    // If there is an error, throw the error
+    if(err) throw err;
+    // Prints out the SQL department table in the console 
+    console.table(result);
+    // Starts the user prompts
+    startPrompts();
+  })
+}
+
+function viewRoles() {
+  // Selects all of the columns from the roles table
+  connection.query('SELECT * FROM role', function(err, result){
+    // If there is an error, throw the error
+    if(err) throw err;
+    // Prints out the SQL role table in the console
+    console.table(result);
+    // Starts the user prompts
+    startPrompts();
+  })
 }
 
 
